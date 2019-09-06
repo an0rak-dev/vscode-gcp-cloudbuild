c903e02729a895fda0a64df257609acd69a5a535
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2019-09-06 23:49:43 +0200

    Add Jest integration to the project

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,26 @@
 import * as vscode from 'vscode';
 
+/**
+ * Activate is called whenever the current workspace contains a 
+ * "cloudbuild.yaml" file.
+ * 
+ * After activation, the extension will periodically (1000ms) call the `gcloud` 
+ * command to get the list of all the builds for the current branch, then it 
+ * will display the most recent build status in the status bar (and as a tooltip
+ * the timestamp of the build).
+ * 
+ * @param context the execution context of the calling VSCode instance.
+ */
 export function activate(context: vscode.ExtensionContext) {
 	let buildChecker = vscode.commands.registerCommand('extension.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World!');
 	});
-
 	context.subscriptions.push(buildChecker);
 }
 
-// this method is called when your extension is deactivated
+/**
+ * Deactivate is called whenever the extensions is disabled or uninstalled.
+ * 
+ * It will also remove the interval function which pings `gcloud`.
+ */
 export function deactivate() {}
---
+++ tests/extension.test.ts
@@ -0,0 +1,5 @@
+describe('test jest', () => {
+   test('jest test spec', () => {
+      expect(42).toBe(42);
+   });
+});
