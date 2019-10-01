69f1d51110416b1131a9e6459729fa0a3c1357fc
jkaluzka <jkaluzka@gmail.com>
2019-10-01 17:35:01 +0200

    Increasing code coverage for `extension`.
    - adding test coverage report.
    - making Job variables read only.

--- tests/extension.test.ts
+++ tests/extension.test.ts
@@ -14,59 +14,115 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 const vscode = require('./__mocks__/vscode');
-const cloudbuild = require('./__mocks__/cloudbuild');
-import {activate, statusBar, deactivate} from '../src/extension';
-import { StatusBarAlignment } from 'vscode';
+import * as cloudBuild from '../src/cloudbuild';
+import {activate, statusBar, deactivate, refreshTicker} from '../src/extension';
+import {StatusBarAlignment} from 'vscode';
+import {GitRepo} from '../src/git';
 
 describe('extension', () => {
-   let context: any;
+  let context: any;
+  let fetchBuildSpy: any;
+  let getCurrentBranchSpy: any;
 
-   beforeEach(() => {
-      vscode.commands.registerCommand.mockReset();
-      vscode.window.createStatusBarItem.mockReset();
-      vscode.window.createStatusBarItem.mockImplementation((align:StatusBarAlignment, prio:number) => {
-         return {
-            alignment: align,
-            priority: prio,
-            text: '',
-            tooltip: '',
-            show: jest.fn()
-         };
-      });
-      cloudbuild.fetchBuilds.mockReset();
-      context = {
-         subscriptions: {
-            push: jest.fn()
-         }
+  beforeEach(() => {
+    vscode.commands.registerCommand.mockReset();
+    vscode.window.createStatusBarItem.mockReset();
+    vscode.window.createStatusBarItem.mockImplementation((align: StatusBarAlignment, prio: number) => {
+      return {
+        alignment: align,
+        priority: prio,
+        text: '',
+        tooltip: '',
+        show: jest.fn(),
       };
-   });
+    });
+    fetchBuildSpy = jest.spyOn(cloudBuild, 'fetchBuilds').mockResolvedValue([]);
+    getCurrentBranchSpy = jest.spyOn(GitRepo.prototype, 'getCurrentBranch').mockResolvedValue('master');
+    context = {
+      subscriptions: {
+        push: jest.fn(),
+      },
+    };
+  });
 
-   afterEach(() => {
-      deactivate();
-   });
+  afterEach(() => {
+    deactivate();
+  });
 
-   describe('activate', () => {
-      test('should set the status bar', () => {
-         // When
-         activate(context);
-         // Then
-         expect(vscode.window.createStatusBarItem.mock.calls).toHaveLength(1);
-         expect(vscode.window.createStatusBarItem.mock.calls[0][0]).toBe(vscode.StatusBarAlignment.Left);
-         expect(vscode.window.createStatusBarItem.mock.calls[0][1]).toBeGreaterThanOrEqual(1000);
-         expect(statusBar).toBeDefined();
+  describe('activate', () => {
+    test('should set the status bar', () => {
+      // When
+      activate(context);
+      // Then
+      const statusItemCalls = vscode.window.createStatusBarItem.mock.calls;
+      expect(statusItemCalls).toHaveLength(1);
+      expect(statusItemCalls[0][0]).toBe(vscode.StatusBarAlignment.Left);
+      expect(statusItemCalls[0][1]).toBeGreaterThanOrEqual(1000);
+      expect(statusBar).toBeDefined();
+    });
+
+    test('should periodically fetch the builds with folders in workspace', async () => {
+      getCurrentBranchSpy = jest.spyOn(GitRepo.prototype, 'getCurrentBranch').mockResolvedValue('develop');
+      jest.useFakeTimers();
+      activate(context);
+      await jest.runOnlyPendingTimers();
+      expect(fetchBuildSpy).toHaveBeenCalledWith('develop');
+    });
+
+    test('should periodically fetch the builds when no opened folder', () => {
+      vscode.workspace = {};
+      jest.useFakeTimers();
+      activate(context);
+      jest.runOnlyPendingTimers();
+      expect(fetchBuildSpy).toHaveBeenCalledWith('master');
+    });
+
+    describe('should display the good status bar label depending the job\'s status', () => {
+      test('should periodically fetch the builds with SUCCESS Job', () => {
+        const job = new cloudBuild.Job(cloudBuild.JobStatus.SUCCESS, new Date());
+        fetchBuildSpy = fetchBuildSpy.mockResolvedValue([job]);
+        jest.useFakeTimers();
+        activate(context);
+        jest.runOnlyPendingTimers();
+        expect(fetchBuildSpy).toHaveBeenCalledWith('master');
+        expect(statusBar).toBeTruthy();
       });
 
-/* TODO: Finish thoses
-      test('should periodically fetch the builds', () => {});
-      test('should display the good status bar label depending the job\'s status', () => {});
-      test('should set the tooltip of the status bar with the time elapsed since last job', () => {});
-*/
-   });
+      test('should periodically fetch the builds with FAILURE Job', () => {
+        const job = new cloudBuild.Job(cloudBuild.JobStatus.FAILURE, new Date());
+        fetchBuildSpy = fetchBuildSpy.mockResolvedValue([job]);
+        jest.useFakeTimers();
+        activate(context);
+        jest.runOnlyPendingTimers();
+        expect(fetchBuildSpy).toHaveBeenCalledWith('master');
+        expect(statusBar).toBeTruthy();
+      });
 
-   describe('deactivate', () => {
-/* TODO: Finish thoses
-      test('should unset the statusbar', () => {});
-      test('should stop the ping interval', () => {});
-*/
-   });
+      test('should periodically fetch the builds with PENDING Job', () => {
+        const job = new cloudBuild.Job(cloudBuild.JobStatus.PENDING, new Date());
+        fetchBuildSpy = fetchBuildSpy.mockResolvedValue([job]);
+        jest.useFakeTimers();
+        activate(context);
+        jest.runOnlyPendingTimers();
+        expect(fetchBuildSpy).toHaveBeenCalledWith('master');
+        expect(statusBar).toBeTruthy();
+      });
+    });
+
+    /* TODO: current specs are running all branches of the `extension` script.
+             They are missing checks for `statusBar` due to the keeping variable
+             in a global state.
+    */
+  });
+
+  describe('deactivate', () => {
+    test('should unset the statusbar and clear Interval', () => {
+      const clearSpy = spyOn(global, 'clearInterval').and.callThrough();
+      activate(context);
+      expect(statusBar).toBeTruthy();
+      deactivate();
+      expect(statusBar).toBeFalsy();
+      expect(clearSpy).toHaveBeenCalledWith(refreshTicker);
+    });
+  });
 });
