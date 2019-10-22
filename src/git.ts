b5eed4eefc7a9e4cb5d9bcceced69afb3135e3b5
jkaluzka <jkaluzka@gmail.com>
2019-10-22 15:17:20 +0200

    Increasing code coverage for `git` repo.
    Includes fixing formatting of `git.ts` file.

--- src/git.ts
+++ src/git.ts
@@ -22,30 +22,31 @@ const HEAD_TAG = 'ref: refs/heads/';
  * GitRepo represents a Git Repository in the local filesystem.
  */
 export class GitRepo {
-   private readonly _gitPath : string;
-   /**
-    * Create a new instance of GitRepo which points to the git repository
-    * at the given path (the root where lives the `.git` folder).
-    * 
-    * @param pth the path of the git repository
-    */
-   constructor(pth: string) {
-      this._gitPath = path.join(pth, '.git');
-   }
+  private readonly _gitPath: string;
 
-   /**
-    * Return the current branch of the calling Git Repository.
-    * 
-    * @returns a Promises which resolves to the branch name's or send an 
-    * error if the path of this GitRepo doesn't points to an actual repository.
-    */
-   getCurrentBranch() : Promise<string> {
-      return new Promise((resolve, reject) => {
-         let headPath = path.join(this._gitPath, 'HEAD');
-         fs.readFile(headPath, "utf-8", (err, data) => {
-            if (err) { reject(err); } 
-            else { resolve(data.replace(HEAD_TAG, '').replace('\n', ''));}
-         });     
+  /**
+   * Create a new instance of GitRepo which points to the git repository
+   * at the given path (the root where lives the `.git` folder).
+   *
+   * @param pth the path of the git repository
+   */
+  constructor(pth: string) {
+    this._gitPath = path.join(pth, '.git');
+  }
+
+  /**
+   * Return the current branch of the calling Git Repository.
+   *
+   * @returns a Promises which resolves to the branch name's or send an
+   * error if the path of this GitRepo doesn't points to an actual repository.
+   */
+  getCurrentBranch(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let headPath = path.join(this._gitPath, 'HEAD');
+      fs.readFile(headPath, 'utf-8', (err, data) => {
+        if (err) { reject(err); }
+        else { resolve(data.replace(HEAD_TAG, '').replace('\n', ''));}
       });
-   }
+    });
+  }
 }
